d20b8cb0027ed93db871d675301dadc2a3b5488a
YUluo <zhaoyiwei.yuluo@bytedance.com>
2019-02-20 18:31:49 +0800

    更新了actiity和fragment的周期过程

--- android-learn-4/app/src/main/java/com/example/yuluo/android_learn_4_fragment/MainActivity.java
+++ android-learn-4/app/src/main/java/com/example/yuluo/android_learn_4_fragment/MainActivity.java
@@ -1,19 +1,66 @@
 package com.example.yuluo.android_learn_4_fragment;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = "RightFragment";
+    //生命周期有关函数的重载
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"activity onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"activity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"activity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"activity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"activity onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"activity onRestart");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.i(TAG,"activity onNewIntent");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"activity onCreate");
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
         replaceFragment(new RightFragment());
